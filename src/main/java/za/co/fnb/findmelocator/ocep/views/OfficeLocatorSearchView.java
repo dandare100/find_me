06044ad24d8d9aea10f5b03171a841caaeef8b7e
valentine <valentine@FOLUF4904672LL1>
2017-10-03 17:44:52 +0200

    Add OfficeLocatorSearchView Deleted OfficeLocatorView Edit config

---
+++ src/main/java/za/co/fnb/findmelocator/ocep/views/OfficeLocatorSearchView.java
@@ -0,0 +1,19 @@
+package za.co.fnb.findmelocator.ocep.views;
+
+import za.co.fnb.pe.framework.util.views.ISView;
+
+/**
+ * @author Valentine Nkosi (F4904672)
+ * @Since 03 Oct 2017
+ * @version 1.0
+ */
+public class OfficeLocatorSearchView extends ISView {
+
+	/**
+	 * 
+	 */
+	public OfficeLocatorSearchView() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
